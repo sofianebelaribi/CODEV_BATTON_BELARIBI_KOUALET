dddbf9166d98df116b411932b0a39ca8ea1d98c4
sofiane belaribi <sofiane.belaribi@etu.univ-lyon1.fr>
2019-12-06 11:45:14 +0100

    Added mysql docker

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'root',
               password : '',
-              database : 'test'
+              database : 'database'
             });
 
 connection.connect();
